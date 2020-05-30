23e614f0901e7e70557644419752af9887c0eec7
mktyagi <mktyagi108@gmail.com>
2020-05-30 11:33:26 +0530

    tjos

--- mix.exs
+++ mix.exs
@@ -54,12 +54,4 @@ defmodule Hello.MixProject do
   #     $ mix setup
   #
   # See the documentation for `Mix` for more info on aliases.
-  defp aliases do
-    [
-      setup: ["deps.get", "ecto.setup", "cmd npm install --prefix assets"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-      "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate", "test"]
-    ]
-  end
-end
+
